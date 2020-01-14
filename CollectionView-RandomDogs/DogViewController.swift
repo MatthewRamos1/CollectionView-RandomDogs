6668d99fe1c8c61d946195c1ca7e9a8d94fabf51
Matthew Ramos <mmramos309@gmail.com>
2020-01-14 11:57:08 -0500

    Set item size delegation

--- CollectionView-RandomDogs/DogViewController.swift
+++ CollectionView-RandomDogs/DogViewController.swift
@@ -12,21 +12,56 @@ class DogViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    private var dogImages = [DogImage]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .orange
+        fetchDogImages()
+    }
+    
+    private func fetchDogImages() {
+        DogAPIClient.fetchDogs { [weak self] (result) in
+            switch result {
+            case .failure(let appError):
+                print("could not fetch dog images with error: \(appError)")
+            case .success(let dogImages):
+                self?.dogImages = dogImages
+            }
+        }
     }
 }
 
 extension DogViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        50
+        dogImages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "dogCell", for: indexPath) as? DogCell else {
             fatalError("Could not downcast to dog cell")
         }
+        let dogImage = dogImages[indexPath.row]
+        cell.configureCell(with: dogImage)
         return cell
+    }
+}
+
+extension DogViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let interItemSpacing: CGFloat = 10
+        let maxWidth = UIScreen.main.bounds.size.width
+        let numberOfItems: CGFloat = 3
+        let totalSpacing: CGFloat = numberOfItems * interItemSpacing
+        let itemWidth: CGFloat = (maxWidth - totalSpacing) / numberOfItems
+        
+        return CGSize(width: itemWidth, height: itemWidth)
     }
 }
